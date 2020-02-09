b99cbda63634695d4d30d1e8bfbf92f717319a2b
James Minton <james-minton@github.com>
2020-02-09 01:22:52 -0500

    Add states form, return Google sheets data

--- themes/base-hugo-theme/static/js/table2.js
+++ themes/base-hugo-theme/static/js/table2.js
@@ -2,58 +2,56 @@
     var jQuery = $.noConflict(false);
     var $ = jQuery;
 
+    /**
+ * Update URL on click
+ */
     
-   /* console.log("Hello world!"); 
-
-        var spreadsheetId = "1ZWf7zFLkcRwSVYhleN_7eFHZ8vxnnjykTwLFMyH9s-k",
-        url = "https://spreadsheets.google.com/feeds/list/1ZWf7zFLkcRwSVYhleN_7eFHZ8vxnnjykTwLFMyH9s-k/od6/public/basic?alt=json";
-
-    $.getJSON({
-        url: url,
-        success: function(response) {
-            console.log(response);
-                
-        }
-        
-        });
-    var obj = JSON.parse(entry);
-    document.getElementById("demo").innerHTML =
-    obj.resource[1].title;   */
         
     $(document).ready(function () {
-      var SHEET_BASE =  'https://spreadsheets.google.com/feeds/list/1ZWf7zFLkcRwSVYhleN_7eFHZ8vxnnjykTwLFMyH9s-k/';
+      var SHEET_BASE =  'https://spreadsheets.google.com/feeds/list/1ovKReDiQ2Qz1UKbbdnCFyz8gIGdm5ik0m3leKcV38Ts/';
       var SHEET_END = '/public/values?alt=json';
-      const SHEET_1_URL = SHEET_BASE + '1' + SHEET_END;
-    // const SHEET_2_URL = SHEET_BASE + '2' + SHEET_END;
-        
-        
-        $('#ak').click(function () {
-          SHEET_NUMBER = 1
+  
+        $('a.state').click(function () {
           $('#show-data').empty();
-           getState();
+          var stateID = $(this).attr('id');
+          var stateNum = stateID.substr(2, 1);
+           getState(stateNum);
         });
-        $('#al').click(function () {
-          SHEET_NUMBER = 2
-            $('#show-data').empty();
-            getState();
-         });
 
-        function getState(){
+        $('.submit-state').click(function () {
+          $('#show-data').empty();
+          var stateID = $( "#states" ).val();
+          var stateNum = stateID.substr(2, 1);
+           getState(stateNum);
+        });
+      
+        function getState(SHEET_NUMBER){ // Loads state's data when triggered by clicking
           var SHEET_URL = SHEET_BASE + SHEET_NUMBER + SHEET_END;
           $.getJSON(SHEET_URL, function (data) {
             var resource = data.feed.entry
             for (var i = 0; i < resource.length; i++) {
-              var rname = resource[i].gsx$resourcename.$t
-              var location = resource[i].gsx$location.$t
-              var link = resource[i].gsx$link.$t
-              var content = '<div class="col-12 col-md-4 col-xl-3">' + rname + '<br>' + location + '<br>' + link + '</div>'
+              var icons = resource[i].gsx$icons.$t
+              var la = icons.includes("la");
+              var icon1 = ""
+              if (la == true) {
+                var icon1 = "Legal Aid"
+              };
+              var rname = resource[i].gsx$organization.$t
+              var location = resource[i].gsx$city.$t
+              var link = resource[i].gsx$website.$t
+              var wrapLink = "<a target='_blank' href='" + link + "'>Website</a>";
+              var content = '<div class="col-12 col-md-4 col-xl-3">' + rname + '<br>' + location + 
+              '<br>' + wrapLink + '<br>' + icon1 + '</div>'
               var showData = $('#show-data');
               showData.append(content);
             };   
          });
         };  
+
+
     });
 
+   
  
 
 })(jQuery);
