92ca39908b640647d75b567ccae5bea9fc435ba6
James Minton <james-minton@github.com>
2020-02-10 17:12:18 -0500

    Add filtering

--- themes/base-hugo-theme/static/js/table2.js
+++ themes/base-hugo-theme/static/js/table2.js
@@ -1,12 +1,7 @@
 (function($) {
     var jQuery = $.noConflict(false);
     var $ = jQuery;
-
-    /**
- * Update URL on click
- */
     
-        
     $(document).ready(function () {
       var SHEET_BASE =  'https://spreadsheets.google.com/feeds/list/1ovKReDiQ2Qz1UKbbdnCFyz8gIGdm5ik0m3leKcV38Ts/';
       var SHEET_END = '/public/values?alt=json';
@@ -32,25 +27,52 @@
             for (var i = 0; i < resource.length; i++) {
               var icons = resource[i].gsx$icons.$t
               var la = icons.includes("la");
-              var icon1 = ""
-              if (la == true) {
-                var icon1 = "Legal Aid"
+              var ea = icons.includes("ea");
+              var ha = icons.includes("ha");
+              var tr = icons.includes("tr");
+              var icon1 = ''; var icon2 = ''; var icon3 = ''; var icon4 = '';
+              var class1 = ''; var class2 = ''; var class3 = ''; var class4 = '';
+              if (la === true) {
+                class1 = 'la '
+                var icon1 = "Legal Aid<br>"
+              };
+              if (ea === true) {
+                class2 = 'ea '
+                var icon2 = "Education & Advocacy<br>"
+              };
+              if (ha === true) {
+                class3 = 'ha '
+                var icon3 = "Housing Assistance<br>"
+              };
+              if (tr === true) {
+                class4 = 'tr '
+                var icon4 = "Tenant Rights"
               };
               var rname = resource[i].gsx$organization.$t
               var location = resource[i].gsx$city.$t
               var link = resource[i].gsx$website.$t
               var wrapLink = "<a target='_blank' href='" + link + "'>Website</a>";
-              var content = '<div class="col-12 col-md-4 col-xl-3">' + rname + '<br>' + location + 
-              '<br>' + wrapLink + '<br>' + icon1 + '</div>'
+              var content = '<div class="state-data col-12 col-md-4 col-xl-3 ' + class1 + class2 + class3 + class4 + '">' + rname + '<br>' + location + 
+              '<br>' + wrapLink + '<br>' + icon1 + icon2 + icon3 + icon4 + '</div>';
+              if (icon1 === 'Legal Aid') {
+               console.log('check'); 
+               $('div').addClass('la');
+              };
               var showData = $('#show-data');
               showData.append(content);
+              
             };   
          });
         };  
 
 
     });
-
+    $("#filters :checkbox").click(function() {
+      $("div.state-data").hide();
+      $("#filters :checkbox:checked").each(function() {
+          $("." + $(this).val()).show();
+      });
+   });
    
  
 
