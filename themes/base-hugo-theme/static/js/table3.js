9b067aa4986c65587def5ce5ebce50fed3405620
James Minton <james-minton@github.com>
2020-03-06 15:10:23 -0500

    Update map

--- themes/base-hugo-theme/static/js/table3.js
+++ themes/base-hugo-theme/static/js/table3.js
@@ -3,23 +3,11 @@
     var $ = jQuery;
     
     $(document).ready(function () {
-      var SHEET_BASE =  'https://spreadsheets.google.com/feeds/list/1ovKReDiQ2Qz1UKbbdnCFyz8gIGdm5ik0m3leKcV38Ts/';
+      var SHEET_BASE =  'https://spreadsheets.google.com/feeds/list/1cRbxIHtRrGw4iZMb4m07a54_1l3wFl6ovWEPVfoPYjg/';
       var SHEET_END = '/public/values?alt=json';
-
-        //Map-based selection of states
-        $('svg g').click(function () {
-          $('#show-data').empty()
-          .css({"opacity":"0"})
-          .stop(true,false)
-          .animate({ 
-          opacity: "1"
-          }, 2400); 
-          var stateID = $(this).attr('id');
-          var stateNum = stateID.substr(2);
-           getState(stateNum);
-        });
-
-        //Dropdown menu selection of states
+      var sheetNum = 1;
+      getStates(sheetNum);
+      /*  //Dropdown menu selection of states
         $('.submit-state').click(function () {
           $('#show-data').empty()
           .css({"opacity":"0","left":"100%"})
@@ -31,17 +19,22 @@
           var stateID = $( "#states" ).val();
           var stateNum = stateID.substr(2);
            getState(stateNum);
-        });
-      
-        function getState(SHEET_NUMBER){ // Loads state's data from Google Sheet when triggered by clicking
+        }); */
+        function getStates(SHEET_NUMBER){ // Loads state's data from Google Sheet when triggered by clicking
           // Use SHEET_NUMBER to identify the relevant sheet tab for each state, e.g. Alaska = 1
           var SHEET_URL = SHEET_BASE + SHEET_NUMBER + SHEET_END; 
           // Get the Google Sheets JSON feed for this state
           $.getJSON(SHEET_URL, function (data) {
-              var resource = data.feed.entry
-              for (var i = 0; i < resource.length; i++) {
+            var resource = data.feed.entry; var totalCount = resource.length
+            console.log(totalCount);
+            var res = {};
+            resource.forEach(function(v) {
+              res[v.gsx$state.$t] = (res[v.gsx$state.$t] || 0) + 1;
+            })
+            for (var i = 0; i < resource.length; i++) {
               // Set variable for value in Icons column  
               var icons = resource[i].gsx$icons.$t
+
               // Set icon variables to empty string
               var icon1 = ''
               var icon2 = ''
@@ -51,7 +44,8 @@
               var class1 = '' 
               var class2 = ''
               var class3 = '' 
-              var class4 = '' 
+              var class4 = ''
+              var class5 =  resource[i].gsx$state.$t
               // If icons column cell contains "la" ...
               var la = icons.includes("la");
               if (la === true) {
@@ -81,17 +75,36 @@
               var link = resource[i].gsx$website.$t
               // Set up rendering of data on front end
               var wrapLink = "<a target='_blank' href='" + link + "'>Website</a>";
-              var content = '<div class="state-data col-12 col-md-4 col-xl-3 ' + class1 + class2 + class3 + class4 + '">' + rname + '<br>' + location + 
+              var content = '<div class="state-data col-12 col-md-4 col-xl-3 ' + class1 + class2 + class3 + class4 + class5 + '">' + rname + '<br>' + location + 
               '<br>' + wrapLink + '<br>' + icon1 + icon2 + icon3 + icon4 + '</div>';
               // Append rendered data to a div
               var showData = $('#show-data');
-              showData.append(content); 
+              $(showData).hide();  
+              $(showData).append(content);
+            }; // for loop
+            $('svg g').click(function () {
+              /*.css({"opacity":"0"})
+              .stop(true,false)
+              .animate({ 
+              opacity: "1"
+              }, 2400); */
+              var stateID = $(this).attr('id');
+              var stateCode = stateID.substr(0,2);
+              var stateCodeUpper = stateCode.toUpperCase();
+              var stateCount = res[stateCodeUpper]
+              console.log(stateCount + ' resources');
               // Clear out filter checkboxes
-              $(".services-filter").prop("checked", false);      
-            };   
-          });
-        };  
-    });
+              $(".services-filter").prop("checked", false);  
+              $('#show-data').show();
+              $('div.state-data').each(function() {
+                $('div.state-data').hide();
+                var stateDiv = ".state-data." + stateCodeUpper;
+                $(stateDiv).show();
+              });
+            }); // svg click function
+          }); //getJson
+        };  // getstate
+    }); //document ready
     // Set up filtering functionality on front end
     $("#filters :checkbox").click(function() {
       $("div.state-data").hide();
@@ -99,6 +112,8 @@
           $("." + $(this).val()).show();
       });
    });
+   //Map-based selection of states
+   
    
 // CLP ----------------------------------------------------------------
   $(document).ready(function () {
